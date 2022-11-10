4eecf23e3c291a9d4f4c49281fa26a0188633777
Melquisedeque <melck_junior@hotmail.com>
2022-11-10 14:32:11 +0000

    Project created

---
+++ src/main/java/com/melck/carrertoolms/CarrerToolMsApplication.java
@@ -0,0 +1,13 @@
+package com.melck.carrertoolms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrerToolMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrerToolMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melck/carrertoolms/CarrerToolMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melck.carrertoolms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerToolMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
