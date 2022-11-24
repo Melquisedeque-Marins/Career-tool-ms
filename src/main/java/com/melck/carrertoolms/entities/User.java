aa106221ac1fc2ec98d514b822ef88d65041ef29
Melquisedeque <melck_junior@hotmail.com>
2022-11-24 13:27:34 +0000

    Application.yml and User model

---
+++ src/main/java/com/melck/carrertoolms/entities/User.java
@@ -0,0 +1,30 @@
+package com.melck.carrertoolms.entities;
+
+import com.melck.carrertoolms.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Gender gender;
+    private String phoneNumber;
+    private String email;
+
+}
---
+++ src/main/java/com/melck/carrertoolms/enums/Gender.java
@@ -0,0 +1,6 @@
+package com.melck.carrertoolms.enums;
+
+public enum Gender {
+
+    FEMALE, MALE;
+}
