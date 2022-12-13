5e54b0d23043a44e521ba70241747cc910065ad9
Melquisedeque <melck_junior@hotmail.com>
2022-12-13 19:23:18 +0000

    Swagger

---
+++ src/main/java/com/melck/carrertoolms/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.melck.carrertoolms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Component
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.melck.doctor.ms"))
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Doctor Label API")
+                .description("Spring Boot REST API for Career tool")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact () {
+        return new Contact("Melquisedeque Marins"
+                , "https://github.com/Melquisedeque-Marins"
+                , "melck_junior@hotmail.com");
+    }
+
+
+}
